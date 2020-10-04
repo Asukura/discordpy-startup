6cbf3eeaba695d93bf5b1047de6460290ae6f653
Asukura <72333443+Asukura@users.noreply.github.com>
2020-10-04 18:44:23 +0900

    Update rollcall_edit.py

--- rollcall/rollcall_edit.py
+++ rollcall/rollcall_edit.py
@@ -8,9 +8,7 @@ with open("rollcall_data.txt",encoding="utf_8_sig") as rf:
         readfile = rf.readlines()
         
         for read_text in readfile:
-            # listに分割
             read_text = read_text.split()
-            # csvに書き込む
             writer = csv.writer(wf, delimiter=',')
             writer.writerow(read_text)
 
