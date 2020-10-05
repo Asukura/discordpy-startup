c6f2689e57692d3c925d20595f002f2319a98de4
Asukura <72333443+Asukura@users.noreply.github.com>
2020-10-05 12:10:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,11 +11,12 @@ client = discord.Client()
 async def on_message(message):
     if message.author.bot:
         return
+    
     elif message.content == "output csv":
         client.send_message(message.channel,LIST)
+        
     elif message.content:
         dt_now = datetime.datetime.now()
-        
         LIST.append(str(dt_now.year + " ") + str(dt_now.month+ " ") + str(dt_now.day+ " ") + message.author.name)
     
 
