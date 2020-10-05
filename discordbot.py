6572a6aab59a41d804a603763b615e428ed019c1
Asukura <72333443+Asukura@users.noreply.github.com>
2020-10-05 12:14:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,10 +11,14 @@ async def on_message(message):
     if message.author.bot:
         return
     
-    if message.content:
+    if message.content == "output csv":
+        client.send_message(message.channel,LIST)
+
+    else:
         dt_now = datetime.datetime.now()
         with open("rollcall/rollcall_data.txt","a",encoding="utf_8") as file:
             file.write(str(dt_now.year + " ") + str(dt_now.month+ " ") + str(dt_now.day+ " ") + message.author.name + "\n")
+        LIST.append(str(dt_now.year + " ") + str(dt_now.month+ " ") + str(dt_now.day+ " ") + message.author.name)
 
 
 client.run(TOKEN)
