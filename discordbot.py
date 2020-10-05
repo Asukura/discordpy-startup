0828d459d0064201f0663ffa8b2bd3d111e3e299
Asukura <72333443+Asukura@users.noreply.github.com>
2020-10-05 12:22:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,12 +11,12 @@ async def on_message(message):
     if message.author.bot:
         return
     
-    if message.content:
-        dt_now = datetime.datetime.now()
-        LIST.append(str(dt_now.year + " ") + str(dt_now.month+ " ") + str(dt_now.day+ " ") + message.author.name)
-        if message.content.startswith("はろー"):
-            m = "こんにちは、" + message.author.name + "さん"
-            await client.send_message(message.channel,m)
+    #if message.content:
+        #dt_now = datetime.datetime.now()
+        #LIST.append(str(dt_now.year + " ") + str(dt_now.month+ " ") + str(dt_now.day+ " ") + message.author.name)
+    if message.content.startswith("はろー"):
+        m = "こんにちは、" + message.author.name + "さん"
+        await client.send_message(message.channel,m)
     
 
 
